a556a41b2baf595787cf3e7d4e4f0fbf19fac5b0
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-12-19 17:21:46 -0300

    inicio backend

---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require("./src/server");
+
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`%s listening at ${port}`)
+})
---
+++ src/server.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+const server = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const multer = require("multer");
+
+
+server.use(morgan("dev"));
+server.use(express.urlencoded({ extended: false }));
+server.use(cors());
+//!DEPLOY server.use(cors(corsOptions));
+server.use(express.json());
+// server.use(fileUpload({
+//     useTempFiles: true,
+//     tempFileDir: './src/public/upload' // ./upload/
+// }));
+
+module.exports = server;
