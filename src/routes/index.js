7499431dd62d27b47892dfcd63d44ed8a19ae5e0
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2024-01-29 14:24:09 -0300

    act

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,9 @@ const { Router } = require("express");
 const publicationsRoutes = require("./publicationsRoutes")
 const categoryRoutes = require("./categoryRoutes")
 const testimonialsRoutes = require("./testimonialsRoutes")
-const paymentRoutes = require("./paymentRoutes")
 
 const authRoutes = require("./authRoutes");
-const ordersRoutes = require('./ordersRoutes')
+
 
 const router = Router();
 
@@ -15,8 +14,7 @@ router.use("/auth", authRoutes);
 router.use("/publications", publicationsRoutes);
 router.use("/category", categoryRoutes);
 router.use("/testimonials", testimonialsRoutes);
-router.use("/payment", paymentRoutes);
-router.use("/orders", ordersRoutes);
+
 
 
 module.exports = router;
