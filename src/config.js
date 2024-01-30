a1830605d89c2662f66966a761be7b969350f1ca
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2024-01-30 15:56:48 -0300

    vercel

--- src/config.js
+++ src/config.js
@@ -1,17 +1,8 @@
 
- 
- const dotenv = require('dotenv');
- dotenv.config();
-
-
- 
- const PAYPAL_API_CLIENT =process.env.PAYPAL_API_CLIENT
- const PAYPAL_API_SECRET =process.env.PAYPAL_API_SECRET
 PAYPAL_API = 'https://api-m.sandbox.paypal.com';
 
 
 module.exports = {
     PAYPAL_API,
-    PAYPAL_API_CLIENT,
-    PAYPAL_API_SECRET
+   
 }
